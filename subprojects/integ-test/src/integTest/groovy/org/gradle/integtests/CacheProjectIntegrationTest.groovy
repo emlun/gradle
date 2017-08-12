8d64258c5bd58d2ea318c939dcc988d43b86c97f
Lóránt Pintér <lorant@gradle.com>
2017-08-12 11:40:06 +0200

    Move FileHasher to base-services

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/CacheProjectIntegrationTest.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/CacheProjectIntegrationTest.groovy
@@ -17,9 +17,9 @@
 package org.gradle.integtests
 
 import org.gradle.api.internal.artifacts.ivyservice.CacheLayout
-import org.gradle.api.internal.hash.FileHasher
 import org.gradle.api.internal.hash.TestFileHasher
 import org.gradle.integtests.fixtures.AbstractIntegrationTest
+import org.gradle.internal.hash.FileHasher
 import org.gradle.internal.hash.HashUtil
 import org.gradle.test.fixtures.file.TestFile
 import org.gradle.test.fixtures.server.http.HttpServer
